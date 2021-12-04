86d8a836192d00a149eef3e5c2b29d2f5e907585
Goddamnnoob <gowthamreddy547@gmail.com>
2021-12-04 11:07:06 +0530

    start

--- ransomeware/ransomeware.go
+++ ransomeware/ransomeware.go
@@ -1,1 +1,16 @@
 package ransomeware
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func GetAllFiles() {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(homeDir)
+	//files, err := ioutil.ReadDir("")
+}
