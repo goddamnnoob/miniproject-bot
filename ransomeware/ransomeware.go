dbe44bab4b98b2df7498ced200435073075def52
goddamnnoob <gowthamreddy547@gmail.com>
2021-12-04 16:01:18 +0530

    debug

--- ransomeware/ransomeware.go
+++ ransomeware/ransomeware.go
@@ -18,14 +18,21 @@ func GetAllFiles() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		filepath.Join(homeDir, "/debug")
 	}
 	fmt.Println(homeDir)
 	var files []string
 	err = filepath.Walk(homeDir, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() && info.Size() > 20 {
 			files = append(files, path)
-
+			/*data, err := os.ReadFile(path)
+			if err != nil {
+				log.Fatal("Error while reading file")
+			}
 			fmt.Println(path)
+			fmt.Println("----------------------------------------------------------------")
+			fmt.Println(string(data))*/
+
 		}
 		return nil
 	})
@@ -35,7 +42,6 @@ func GetAllFiles() {
 	for _, file := range files {
 		fmt.Println(file)
 	}
-
 	//return files, err
 
 }
